aef13b72d412b5a4044f5674f1fbb77f6d0f12b5
ameer-ahmad <ameer5ahmad@gmail.com>
2022-08-01 22:36:00 -0400

    added view projects animation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import About from './components/About'
+import Projects from './components/Projects'
 
 function App() {
   return (
     <div className="App">
       <About />
+      <Projects />
     </div>
   );
 }
--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import ameer from './ameer.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFile } from '@fortawesome/free-solid-svg-icons'
+import { faFile, faAngleDoubleDown } from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import Typewriter from 'typewriter-effect'
+import resume from './AmeerAhmadSoftwareEngineerResume.pdf'
 
 function About() {
   return (
@@ -33,13 +34,17 @@ function About() {
             <ul className="aboutSocials">
                 <li><a href="https://github.com/ameer-ahmad" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub} /></a></li>
                 <li><a href="https://www.linkedin.com/in/ameerahmad/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faLinkedin} /></a></li>
-                <li><FontAwesomeIcon icon={faFile} /></li>
+                <li><a href={resume} target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faFile} /></a></li>
             </ul>
         </div>
         <div className="aboutRight">
             <img className="aboutImg" src={ameer} alt="Ameer Ahmad" />
             <p>Welcome to my portfolio! <br/><br/> I am a Software Engineering student at Sheridan College who loves to create dynamic web apps that solve problems and make people's lives easier. <br/><br/> On this website you can view my past experiences, as well as all
             my personal projects. Currently, I'm looking for Fall 2022 internships. Feel free to contact me on LinkedIn or through email.</p>
+        </div>
+        <div className="seeProjects">
+            <p>View Projects</p>
+            <FontAwesomeIcon icon={faAngleDoubleDown} className="bounce" />
         </div>
         
     </div>
