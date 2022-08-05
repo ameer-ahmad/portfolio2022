bb481bcdb05e4b7ce75ab88d0242de79b8352d23
ameer-ahmad <ameer5ahmad@gmail.com>
2022-08-05 17:18:25 -0400

    created experiences section

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import experiences from '../data/experienceData'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBriefcase } from '@fortawesome/free-solid-svg-icons'
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component'
+import "react-vertical-timeline-component/style.min.css"
 
 function Experience() {
   return (
-    <div className="experience" id="experience">Experience</div>
+    <div className="experience" id="experience">
+      <h2>Experience <FontAwesomeIcon icon={faBriefcase}/></h2>
+      <VerticalTimeline>
+        { 
+          experiences.map(element => {
+            return (
+              <VerticalTimelineElement key={element.key} date={element.date} dateClassName="date" icon={<FontAwesomeIcon icon={faBriefcase} />} >
+                <h3 className="vertical-timeline-element-title">{element.title} - {element.company}</h3>
+                <h5 className="vertical-timeline-element-subtitle">{element.location}</h5>
+                <p id="description">{element.desc}</p>
+              </VerticalTimelineElement>
+            )
+          })
+        }
+      </VerticalTimeline>
+    </div>
   )
 }
 
---
+++ src/data/experienceData.js
@@ -0,0 +1,39 @@
+const experiences = [
+    {
+        company: "Aceit Digital Inc.",
+        title: "Web Developer",
+        location: "Remote",
+        date: "June 2022 - August 2022",
+        desc: ""
+    },
+    {
+        company: "Pocketed Grant Platform",
+        title: "Software Developer",
+        location: "Remote",
+        date: "January 2022 - March 2022",
+        desc: ""
+    },
+    {
+        company: "ClearEvent",
+        title: "Front End Developer",
+        location: "Remote",
+        date: "September 2021 - December 2021",
+        desc: ""
+    },
+    {
+        company: "My Media Creative",
+        title: "Web Developer",
+        location: "Remote",
+        date: "April 2021 - August 2021",
+        desc: ""
+    },
+    {
+        company: "Hackademics Club",
+        title: "Frontend Web Developer",
+        location: "Remote",
+        date: "September 2020 - December 2020",
+        desc: ""
+    }
+]
+
+export default experiences
