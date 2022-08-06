6628b032a0d03675d6e64933a04e35c6f61d0550
ameer-ahmad <ameer5ahmad@gmail.com>
2022-08-06 19:33:33 -0400

    styled experience section

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -9,14 +9,18 @@ function Experience() {
   return (
     <div className="experience" id="experience">
       <h2>Experience <FontAwesomeIcon icon={faBriefcase}/></h2>
-      <VerticalTimeline>
+      <VerticalTimeline className="timeline">
         { 
           experiences.map(element => {
             return (
-              <VerticalTimelineElement key={element.key} date={element.date} dateClassName="date" icon={<FontAwesomeIcon icon={faBriefcase} />} >
-                <h3 className="vertical-timeline-element-title">{element.title} - {element.company}</h3>
-                <h5 className="vertical-timeline-element-subtitle">{element.location}</h5>
+              <VerticalTimelineElement className="timelineText" key={element.key} date={element.date} dateClassName="date" iconStyle={{background: 'white'}} icon={<FontAwesomeIcon icon={faBriefcase} />} >
+                <div className="timelineTextContainer">
+                  <h3 className="vertical-timeline-element-title">{element.title}</h3>
+                  <h5 className="vertical-timeline-element-subtitle">{element.location}</h5>
+                </div>
+                <h5 className="vertical-timeline-element-subtitle">{element.company}</h5>
                 <p id="description">{element.desc}</p>
+                <h5 className="vertical-timeline-element-subtitle">{element.date}</h5>
               </VerticalTimelineElement>
             )
           })
--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -4,35 +4,35 @@ const experiences = [
         title: "Web Developer",
         location: "Remote",
         date: "June 2022 - August 2022",
-        desc: ""
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem."
     },
     {
         company: "Pocketed Grant Platform",
         title: "Software Developer",
         location: "Remote",
         date: "January 2022 - March 2022",
-        desc: ""
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem."
     },
     {
         company: "ClearEvent",
         title: "Front End Developer",
         location: "Remote",
         date: "September 2021 - December 2021",
-        desc: ""
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem."
     },
     {
         company: "My Media Creative",
         title: "Web Developer",
         location: "Remote",
         date: "April 2021 - August 2021",
-        desc: ""
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem."
     },
     {
         company: "Hackademics Club",
         title: "Frontend Web Developer",
         location: "Remote",
         date: "September 2020 - December 2020",
-        desc: ""
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem."
     }
 ]
 
