8f5c2fc3df98173852018c2c6904410d41cf8a87
ameer-ahmad <ameer5ahmad@gmail.com>
2022-08-07 12:49:54 -0400

    made header mobile responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCode } from '@fortawesome/free-solid-svg-icons'
+import { faCode, faBars, faXmark } from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-scroll'
 import resume from './AmeerAhmadSoftwareEngineerResume.pdf'
 
 function Header() {
 
     const [isHeaderVisible, setIsHeaderVisible] = useState(false)
+    const [isClicked, setIsClicked] = useState(false)
 
     const handleHeader = () => {
         const aboutBottom = document.getElementById('about').getBoundingClientRect().bottom
@@ -17,6 +18,10 @@ function Header() {
         }
     }
 
+    const handleClick = () => {
+        setIsClicked(!isClicked)
+    }
+
     window.addEventListener('scroll', handleHeader)
 
   return (
@@ -25,7 +30,8 @@ function Header() {
             <FontAwesomeIcon className="logo" icon={faCode}/>
         </Link>
         <nav>
-            <ul>
+            <FontAwesomeIcon className='menu-icon' icon={isClicked ? faXmark : faBars} onClick={handleClick} />
+            <ul className={isClicked ? "active" : ""}>
                 <li><Link activeClass='linkActive' spy={true} to="about" smooth={true} duration={500}>About</Link></li>
                 <li><Link activeClass='linkActive' spy={true} to="projects" smooth={true} duration={500}>Projects</Link></li>
                 <li><Link activeClass='linkActive' spy={true} to="experience" smooth={true} duration={500}>Experience</Link></li>
